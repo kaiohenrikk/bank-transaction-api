12e4b4dce048019d4ee5e6afb90f15015b94a73d
Kaio Melchior <kaio.melchior-terceiro@cea.com.br>
2024-10-23 12:05:05 -0300

    fix: adicionando mock do logger

--- tests/integration/health/controller/health.controller.test.ts
+++ tests/integration/health/controller/health.controller.test.ts
@@ -2,14 +2,27 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../../../../src/app.module';
+import { Logger } from 'winston';
+import { LoggerService } from '../../../../src/common/logger.service';
 
-describe('HealthController (e2e)', () => {
+// Create a mock logger service
+const mockLogger = {
+  info: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  debug: jest.fn(),
+};
+
+describe('HealthController', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+    .overrideProvider(LoggerService) 
+    .useValue(mockLogger)
+    .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
